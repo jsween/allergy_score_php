3651163d5204eb3578db882d6b0723112c23842d
jon sweeney and michael lor <jonsweeney26@gmail.com>
2016-02-16 13:52:35 -0800

    create while loop to give change

--- src/Anagram.php
+++ src/Anagram.php
@@ -1,28 +1,37 @@
 <?php
     class Change
     {
-    
         function giveChange($user_change)
         {
-            $final_change = array();
-            //quarters
-            $quarters = floor($user_change/25);
-            array_push($final_change, $quarters);
-            $remainder = floor($user_change % 25);
-            //dimes
-            $dimes = floor($remainder/10);
-            array_push($final_change, $dimes);
-            $remainder = floor($remainder % 10);
-            //nickels
-            $nickels = floor($remainder/5);
-            array_push($final_change, $nickels);
-            $remainder = floor($remainder % 5);
-            //pennies
-            $pennies = floor($remainder/1);
-            array_push($final_change, $pennies);
-            $remainder = floor($remainder % 1);
+            $final_change = array(0,0,0,0);
+            $currency = array(25, 10, 5, 1);
+            for($i = 0; $i < 4; $i++) {
+                while($user_change > ($currency[$i] - 1)) {
+                    $final_change[$i] += 1;
+                    $user_change -= $currency[$i];
+                }
+            }
 
-        return $final_change;
+        return $final_change[0] . " quarters " . $final_change[1] . " dimes " . $final_change[2] . " nickels " . $final_change[3] . " pennies ";
+            // $final_change = array();
+            // //quarters
+            // $quarters = floor($user_change/25);
+            // array_push($final_change, $quarters);
+            // $remainder = floor($user_change % 25);
+            // //dimes
+            // $dimes = floor($remainder/10);
+            // array_push($final_change, $dimes);
+            // $remainder = floor($remainder % 10);
+            // //nickels
+            // $nickels = floor($remainder/5);
+            // array_push($final_change, $nickels);
+            // $remainder = floor($remainder % 5);
+            // //pennies
+            // $pennies = floor($remainder/1);
+            // array_push($final_change, $pennies);
+            // $remainder = floor($remainder % 1);
+
+        //  return $final_change[0] . " quarters " . $final_change[1] . " dimes " . $final_change[2] . " nickels " . $final_change[3] . " pennies ";
         }
     }
 
--- tests/AnagramTest.php
+++ tests/AnagramTest.php
@@ -4,54 +4,103 @@
 
     class ChangeTest extends PHPUnit_Framework_TestCase
     {
-        function test_for_1_quarter()
+        function test_for_quarters()
         {
             //Arrange
             $test_user_input = new Change;
-            $user_change= 25;
+            $user_change= 75;
 
             //Act
             $result = $test_user_input->giveChange($user_change);
 
             //Assert
-            $this->assertEquals([1,0,0,0], $result);
+            $this->assertEquals([3,0,0,0], $result);
         }
         function test_for_dimes()
         {
             //Arrange
             $test_user_input = new Change;
-            $user_change= 35;
+            $user_change= 70;
 
             //Act
             $result = $test_user_input->giveChange($user_change);
 
             //Assert
-            $this->assertEquals([1,1,0,0], $result);
+            $this->assertEquals([2,2,0,0], $result);
         }
         function test_for_nickels()
         {
             //Arrange
             $test_user_input = new Change;
-            $user_change= 24;
+            $user_change= 65;
 
             //Act
             $result = $test_user_input->giveChange($user_change);
 
             //Assert
-            $this->assertEquals([0,2,0,4], $result);
+            $this->assertEquals([2,1,1,0], $result);
         }
         function test_for_pennies()
         {
             //Arrange
             $test_user_input = new Change;
-            $user_change= 67;
+            $user_change= 4;
 
             //Act
             $result = $test_user_input->giveChange($user_change);
 
             //Assert
-            $this->assertEquals([2,1,1,2], $result);
+            $this->assertEquals([0,0,0,4], $result);
         }
+
+        // function test_for_1_quarter()
+        // {
+        //     //Arrange
+        //     $test_user_input = new Change;
+        //     $user_change= 25;
+        //
+        //     //Act
+        //     $result = $test_user_input->giveChange($user_change);
+        //
+        //     //Assert
+        //     $this->assertEquals([1,0,0,0], $result);
+        // }
+        // function test_for_dimes()
+        // {
+        //     //Arrange
+        //     $test_user_input = new Change;
+        //     $user_change= 35;
+        //
+        //     //Act
+        //     $result = $test_user_input->giveChange($user_change);
+        //
+        //     //Assert
+        //     $this->assertEquals([1,1,0,0], $result);
+        // }
+        // function test_for_nickels()
+        // {
+        //     //Arrange
+        //     $test_user_input = new Change;
+        //     $user_change= 24;
+        //
+        //     //Act
+        //     $result = $test_user_input->giveChange($user_change);
+        //
+        //     //Assert
+        //     $this->assertEquals([0,2,0,4], $result);
+        // }
+        // function test_for_pennies()
+        // {
+        //     //Arrange
+        //     $test_user_input = new Change;
+        //     $user_change= 67;
+        //
+        //     //Act
+        //     $result = $test_user_input->giveChange($user_change);
+        //
+        //     //Assert
+        //     $this->assertEquals([2,1,1,2], $result);
+        // }
 
     }
 
